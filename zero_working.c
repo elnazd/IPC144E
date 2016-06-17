fbf4d3db747656bf924e412786b96c23971be849
Marek Laskowski <marek.laskowski@matrix.senecac.on.ca>
2016-06-17 12:42:47 -0400

    added some comments to zero_working.c

--- zero_working.c
+++ zero_working.c
@@ -10,9 +10,15 @@ Here we use pointers to solve the problem
 #include <stdio.h>
 /*
 the zero function will set the value of variable whose address is passed to 0
+now x is a pointer to an integer int *x
+recall pointers store the addresses of other variables
 */
 void zero(int *x){
+	//notice we now need to dereference x to access its value
+	//the dereference operator is *
+	//ex. to dereference x the syntax is *x 
 	printf("inside zero x is: %d the address of main's x is: %p\n", *x, x);
+	//store 0 at the memory pointed to by *x
 	*x = 0;
 	printf("inside zero x is now: %d the address of main's x is: %p\n", *x, x);
 }
@@ -21,7 +27,7 @@ int main(void){
 	int x = 5;
 
 	printf("the value of x before calling zero(x) is: %d the address of x is: %p\n", x, &x);
-	//call the function
+	//call the function - it expects the ADDRESS of x ... recall the address-of operator &
 	zero(&x);
 	//we want x to be zero now.. is it???
 	printf("the value of x after calling zero(x) is: %d the address of x is: %p\n", x, &x);
