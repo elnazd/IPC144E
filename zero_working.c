c1fcc3d703e33a3a67c050f37cf02e252db29948
Marek Laskowski <marek.laskowski@matrix.senecac.on.ca>
2016-06-17 12:22:54 -0400

    added zero_working example (pointers and functions)

--- zero_working.c
+++ zero_working.c
@@ -1,21 +1,20 @@
 /*
 this program attempts to write a fumnction that accepts a variable
 and sets that variable's value in main() to zero i.e. 0
-NOTE: this version doesn't work yet!
+VERSION 2 -working version
 
-we'll see why.
+Here we use pointers to solve the problem 
 
 */
 
 #include <stdio.h>
 /*
-the zero function SHOULD set the variable that's passed to 0
-this version doesn't work...
+the zero function will set the value of variable whose address is passed to 0
 */
-void zero(int x){
-	printf("inside zero x is: %d the address of x is: %p\n", x, &x);
-	x = 0;
-	printf("inside zero x is now: %d the address of x is: %p\n", x, &x);
+void zero(int *x){
+	printf("inside zero x is: %d the address of main's x is: %p\n", *x, x);
+	*x = 0;
+	printf("inside zero x is now: %d the address of main's x is: %p\n", *x, x);
 }
 
 int main(void){
@@ -23,7 +22,7 @@ int main(void){
 
 	printf("the value of x before calling zero(x) is: %d the address of x is: %p\n", x, &x);
 	//call the function
-	zero(x);
+	zero(&x);
 	//we want x to be zero now.. is it???
 	printf("the value of x after calling zero(x) is: %d the address of x is: %p\n", x, &x);
 	return 0;
